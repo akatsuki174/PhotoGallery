f4be21c69bf955dc58248db3eba6a322890dc062
akatsuki174 <dev2015js@gmail.com>
2020-04-26 18:12:31 +0900

    remove deprecated property

--- app/src/main/java/com/example/photogallery/PhotoDataSource.kt
+++ app/src/main/java/com/example/photogallery/PhotoDataSource.kt
@@ -3,22 +3,19 @@ package com.example.photogallery
 import android.content.ContentUris
 import android.content.Context
 import android.provider.MediaStore
-import android.text.TextUtils
 
 class PhotoDataSource(private val context: Context) {
     companion object {
         private val PHOTO_URI = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
         private const val PHOTO_ID = MediaStore.Images.ImageColumns._ID
         private const val PHOTO_DATE_TAKEN = MediaStore.Images.ImageColumns.DATE_TAKEN
-        private const val PHOTO_FILE_PATH = MediaStore.Images.ImageColumns.DATA
         private const val PHOTO_BUCKET_ID = MediaStore.Images.ImageColumns.BUCKET_ID // フォルダの識別子
         private const val PHOTO_FOLDER_NAME = MediaStore.Images.ImageColumns.BUCKET_DISPLAY_NAME
         private val PHOTO_PROJECTION = arrayOf(
             PHOTO_BUCKET_ID,
             PHOTO_FOLDER_NAME,
             PHOTO_ID,
-            PHOTO_DATE_TAKEN,
-            PHOTO_FILE_PATH
+            PHOTO_DATE_TAKEN
         )
 
         private const val PHOTO_SORT = "$PHOTO_DATE_TAKEN DESC, $PHOTO_ID ASC"
@@ -40,20 +37,14 @@ class PhotoDataSource(private val context: Context) {
             }
             val idIndex = cursor.getColumnIndexOrThrow(PHOTO_ID)
             val dateTakenIndex = cursor.getColumnIndexOrThrow(PHOTO_DATE_TAKEN)
-            val filePathIndex = cursor.getColumnIndexOrThrow(PHOTO_FILE_PATH)
             val bucketIdIndex = cursor.getColumnIndexOrThrow(PHOTO_BUCKET_ID)
             val nameIndex = cursor.getColumnIndexOrThrow(PHOTO_FOLDER_NAME)
 
             while (cursor.moveToNext()) {
                 val id = cursor.getLong(idIndex)
                 val dateTaken = cursor.getLong(dateTakenIndex)
-                val filePath = cursor.getString(filePathIndex)
                 val bucketId = cursor.getString(bucketIdIndex)
                 val name = cursor.getString(nameIndex)
-                if (TextUtils.isEmpty(filePath)) {
-                    // ファイルパスが取得できないケースがある
-                     continue
-                }
                 if (bucketIds.contains(bucketId)) {
                     continue
                 } else {
@@ -66,9 +57,8 @@ class PhotoDataSource(private val context: Context) {
             }
         }
 
-        // 「すべて」フォルダのサムネイルを準備
+        // 「すべて」フォルダのURI
         val uri = if (holders.count() > 0) holders[0].uri else null
-
         // 「すべて」フォルダを用意
         holders.add(0, FolderHolderData(uri = uri,
             name = context.getString(R.string.folder_all),
